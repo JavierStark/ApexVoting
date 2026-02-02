757b332899c7f3e9282bebd29d4941cbd75b97c3
JavierStark <javiertorralbocortes@gmail.com>
2026-02-02 16:00:24 +0100

    feat: worker

--- ApexVoting/AppHost.cs
+++ ApexVoting/AppHost.cs
@@ -1,6 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+
 var cache = builder.AddRedis("cache");
+
+var worker = builder.AddProject<Projects.ApexVotingProcessor>("apexvotingprocessor")
+    .WaitFor(cache)
+    .WithReference(cache);
 
 var api = builder.AddProject<Projects.ApexVotingApi>("apexvotingapi")
     .WaitFor(cache)
